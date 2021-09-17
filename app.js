63b34134a6a85d11ce7fbb552592fac966088736
rodrigologar <rodrigo.logar02@gmail.com>
2021-09-17 16:01:32 -0500

    finished responsive navbar and everything before it

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const nav = document.querySelector("nav");
-const navOffset = document.getElementById("button1").offsetTop;
+const navOffset = document.getElementById("social-icons").offsetTop;
 
 window.addEventListener("scroll", function () {
     const offset = window.pageYOffset;
@@ -11,3 +11,15 @@ window.addEventListener("scroll", function () {
         nav.classList.remove("scroll");
     }
 });
+
+function menuToggle() {
+    const navLinks = document.getElementById("nav-links");
+
+    navLinks.classList.toggle("active");
+}
+
+function deToggle() {
+    const navLinks = document.getElementById("nav-links");
+
+    navLinks.classList.remove("active");
+}
